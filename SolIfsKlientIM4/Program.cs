ee1e5ea215263228c7eb20bb6dc41375b8824f29
profikod <biuro@profikod.pl>
2020-08-24 11:52:38 +0200

    Dodaj pliki projektów.

---
+++ SolIfsKlientIM4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolIfsKlientIM4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
